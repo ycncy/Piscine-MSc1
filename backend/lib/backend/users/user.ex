e7c8c9370e1d95ebb5578f142c8515d87fac0979
rbouzidi <redabouzidi62@yahoo.fr>
2023-10-24 09:30:32 +0200

    ajout du code predefini du framework

---
+++ backend/lib/backend/users/user.ex
@@ -0,0 +1,18 @@
+defmodule Backend.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :username, :string
+    field :email, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email])
+    |> validate_required([:username, :email])
+  end
+end
