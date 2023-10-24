a37b99381a042b117f7ab6ddd52fa840bb778f12
ycncy <yacinetalhaoui13@gmail.com>
2023-10-24 11:34:43 +0200

    Push migrations

---
+++ backend/priv/repo/migrations/20231024091821_update_user_role.exs
@@ -0,0 +1,13 @@
+defmodule Backend.Repo.Migrations.UpdateUserRole do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string, null: false
+      add :email, :string, null: false
+      add :role, :string, null: false
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
