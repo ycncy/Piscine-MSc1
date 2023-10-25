a2992d0531d59ad374bdb265af9a5492604041aa
Meriem <ramdani.meriem2001@gmail.com>
2023-10-25 16:00:22 +0200

    add store

---
+++ frontend/src/components/Clocks.vue
@@ -0,0 +1,24 @@
+<script >
+    export default {
+      methods: {
+        startDateTimer() {
+          this.timer = setInterval(() => {
+            this.date = new Date();
+          }, 1000);
+        },
+        clockIn() {
+          this.startDateTimer();
+        },
+        refresh() {
+          this.date = new Date();
+        },
+        clocks() {
+          this.$router.push({ name: "Clocks" });
+        },
+        }
+    }
+</script>
+
+<template>
+  <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import Router from "@/Router";
+import Store from "@/Store";
 
 const app = createApp(App)
 
 app.use(Router)
+app.use(Store)
 
 app.mount('#app')
