6609c62ca8f84fb4333ed17ce9138a641b78e387
ycncy <yacinetalhaoui13@gmail.com>
2023-10-25 17:26:37 +0200

    Composant authentication fonctionnel

--- frontend/src/services/authentication.service.js
+++ frontend/src/services/authentication.service.js
@@ -6,12 +6,12 @@ const get_user = () => {
     return localStorage.getItem("user");
 }
 const set_user = (user) => { 
-    localStorage.setItem("user", user);
+    localStorage.setItem("user", JSON.stringify(user));
 }
 const delete_user = () => {
     localStorage.removeItem("user");
 }   
-export {
+export const authentication_service = {
     is_logged,
     get_user,
     set_user,
