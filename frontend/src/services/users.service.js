5fe07d0671447bc0c0225ebfe6eab1087ed74f3c
ycncy <yacinetalhaoui13@gmail.com>
2023-10-25 12:03:15 +0200

    Ajout de toutes les fonctions dans users_service

--- frontend/src/services/users.service.js
+++ frontend/src/services/users.service.js
@@ -1,11 +1,10 @@
 import Axios from "@/services/api.service";
-import {HttpStatusCode} from "axios";
 
 const users_url = "/users";
 
 const get_user_by_id = async (user_id) => {
     try {
-        const response = await Axios.get(`${users_url}/${user_id}/ziojfzef`);
+        const response = await Axios.get(`${users_url}/${user_id}`);
         return response.data;
     } catch (error) {
         return {
@@ -16,22 +15,83 @@ const get_user_by_id = async (user_id) => {
 }
 
 const get_user_by_credentials = async (username, email) => {
-    await Axios.get(
-        `${users_url}`,
-        {
-            params: {
-                username: username,
-                email: email
+    try {
+        const response = await Axios.get(
+            `${users_url}`,
+            {
+                params: {
+                    username: username,
+                    email: email
+                }
             }
-        }
-    ).then(response => {
+        );
         return response.data;
-    }).catch(error => {
-        console.log(`Error trying to fetch user by credentials : ${error}`)
-        throw error;
-    });
+    } catch (error) {
+        return {
+            status_code: error.response.status,
+            error: error.message
+        }
+    }
+}
+
+const create_user = async (username, email, role) => {
+    try {
+        const response = await Axios.post(
+            `${users_url}`,
+            {
+                user: {
+                    username: username,
+                    email: email,
+                    role: role
+                }
+            }
+        );
+        return response.data;
+    } catch (error) {
+        return {
+            status_code: error.response.status,
+            error: error.message
+        }
+    }
+}
+
+const update_user = async (username, email, role) => {
+    try {
+        const response = await Axios.put(
+            `${users_url}`,
+            {
+                user: {
+                    username: username,
+                    email: email,
+                    role: role
+                }
+            }
+        );
+        return response.data;
+    } catch (error) {
+        return {
+            status_code: error.response.status,
+            error: error.message
+        }
+    }
+}
+
+const delete_user = async (user_id) => {
+    try {
+        const response = await Axios.delete(`${users_url}/${user_id}`);
+        return response.data;
+    } catch (error) {
+        return {
+            status_code: error.response.status,
+            error: error.message
+        }
+    }
 }
 
 export const users_service = {
-    get_user_by_id, get_user_by_credentials
+    get_user_by_id,
+    get_user_by_credentials,
+    create_user,
+    update_user,
+    delete_user
 }
