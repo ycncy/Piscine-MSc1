69635e6be6d42d3be14bfea4197c79cd00eb6684
Meriem <ramdani.meriem2001@gmail.com>
2023-10-26 12:26:08 +0200

    add sidebar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,24 +3,16 @@
   </header>
 
   <main>
-    <div class="canvas">
-      <Line/>
-    </div>
-    <router-view></router-view>
+    <User />  
   </main>
-  <router-link to="/workingtimes">Working times</router-link>
 </template>
 
 <script>
-import Line from './components/Line.vue'
+import User from './components/User.vue'
 export default {
   name: 'App',
-  components: { Line }
+  components: { User }
 }
 </script>
 <style>
-  body .canvas {
-    width: 600px;
-    height: 600px;
-  }
 </style>
