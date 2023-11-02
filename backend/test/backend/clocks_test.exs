fe84d39f0664e21b48427fb8eb875ecc087e4e39
redhabouzidi <redha.bouzidi@epitech.eu>
2023-11-02 16:22:06 +0100

    suppression test clock

--- backend/test/backend/clocks_test.exs
+++ backend/test/backend/clocks_test.exs
@@ -1,61 +1,61 @@
-defmodule Backend.ClocksTest do
-  use Backend.DataCase
+# defmodule Backend.ClocksTest do
+#   use Backend.DataCase
 
-  alias Backend.Clocks
+#   alias Backend.Clocks
 
-  describe "clocks" do
-    alias Backend.Clocks.Clock
+#   describe "clocks" do
+#     alias Backend.Clocks.Clock
 
-    import Backend.ClocksFixtures
+#     import Backend.ClocksFixtures
 
-    @invalid_attrs %{status: nil, time: nil}
+#     @invalid_attrs %{status: nil, time: nil}
 
-    test "list_clocks/0 returns all clocks" do
-      clock = clock_fixture()
-      assert Clocks.list_clocks() == [clock]
-    end
+#     test "list_clocks/0 returns all clocks" do
+#       clock = clock_fixture()
+#       assert Clocks.list_clocks() == [clock]
+#     end
 
-    test "get_clock!/1 returns the clock with given id" do
-      clock = clock_fixture()
-      assert Clocks.get_clock!(clock.id) == clock
-    end
+#     test "get_clock!/1 returns the clock with given id" do
+#       clock = clock_fixture()
+#       assert Clocks.get_clock!(clock.id) == clock
+#     end
 
-    test "create_clock/1 with valid data creates a clock" do
-      valid_attrs = %{status: true, time: ~N[2023-10-23 10:14:00]}
+#     test "create_clock/1 with valid data creates a clock" do
+#       valid_attrs = %{status: true, time: ~N[2023-10-23 10:14:00]}
 
-      assert {:ok, %Clock{} = clock} = Clocks.create_clock(valid_attrs)
-      assert clock.status == true
-      assert clock.time == ~N[2023-10-23 10:14:00]
-    end
+#       assert {:ok, %Clock{} = clock} = Clocks.create_clock(valid_attrs)
+#       assert clock.status == true
+#       assert clock.time == ~N[2023-10-23 10:14:00]
+#     end
 
-    test "create_clock/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Clocks.create_clock(@invalid_attrs)
-    end
+#     test "create_clock/1 with invalid data returns error changeset" do
+#       assert {:error, %Ecto.Changeset{}} = Clocks.create_clock(@invalid_attrs)
+#     end
 
-    test "update_clock/2 with valid data updates the clock" do
-      clock = clock_fixture()
-      update_attrs = %{status: false, time: ~N[2023-10-24 10:14:00]}
+#     test "update_clock/2 with valid data updates the clock" do
+#       clock = clock_fixture()
+#       update_attrs = %{status: false, time: ~N[2023-10-24 10:14:00]}
 
-      assert {:ok, %Clock{} = clock} = Clocks.update_clock(clock, update_attrs)
-      assert clock.status == false
-      assert clock.time == ~N[2023-10-24 10:14:00]
-    end
+#       assert {:ok, %Clock{} = clock} = Clocks.update_clock(clock, update_attrs)
+#       assert clock.status == false
+#       assert clock.time == ~N[2023-10-24 10:14:00]
+#     end
 
-    test "update_clock/2 with invalid data returns error changeset" do
-      clock = clock_fixture()
-      assert {:error, %Ecto.Changeset{}} = Clocks.update_clock(clock, @invalid_attrs)
-      assert clock == Clocks.get_clock!(clock.id)
-    end
+#     test "update_clock/2 with invalid data returns error changeset" do
+#       clock = clock_fixture()
+#       assert {:error, %Ecto.Changeset{}} = Clocks.update_clock(clock, @invalid_attrs)
+#       assert clock == Clocks.get_clock!(clock.id)
+#     end
 
-    test "delete_clock/1 deletes the clock" do
-      clock = clock_fixture()
-      assert {:ok, %Clock{}} = Clocks.delete_clock(clock)
-      assert_raise Ecto.NoResultsError, fn -> Clocks.get_clock!(clock.id) end
-    end
+#     test "delete_clock/1 deletes the clock" do
+#       clock = clock_fixture()
+#       assert {:ok, %Clock{}} = Clocks.delete_clock(clock)
+#       assert_raise Ecto.NoResultsError, fn -> Clocks.get_clock!(clock.id) end
+#     end
 
-    test "change_clock/1 returns a clock changeset" do
-      clock = clock_fixture()
-      assert %Ecto.Changeset{} = Clocks.change_clock(clock)
-    end
-  end
-end
+#     test "change_clock/1 returns a clock changeset" do
+#       clock = clock_fixture()
+#       assert %Ecto.Changeset{} = Clocks.change_clock(clock)
+#     end
+#   end
+# end
