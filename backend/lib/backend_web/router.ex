dc6c12d5cc503e25c320a522c66391967d97ce3c
Meriem <ramdani.meriem2001@gmail.com>
2023-11-12 22:34:36 +0100

    add page timer

--- backend/lib/backend_web/router.ex
+++ backend/lib/backend_web/router.ex
@@ -2,13 +2,11 @@ defmodule BackendWeb.Router do
   use BackendWeb, :router
 
   pipeline :api do
-    plug CORSPlug, origin: "http://34.155.108.18:8080", headers: ["Authorization", "Content-Type", "X-Custom-Header"], methods: ["GET", "POST", "PUT", "DELETE"]
     plug :accepts, ["json"]
   end
 
   pipeline :admin do
     plug BackendWeb.AdminPlug
-    plug CORSPlug, origin: "http://34.155.108.18:8080", headers: ["Authorization", "Content-Type", "X-Custom-Header"], methods: ["GET", "POST", "PUT", "DELETE"]
     plug :accepts, ["json"]
     plug :fetch_session
     plug :fetch_flash
@@ -17,7 +15,6 @@ defmodule BackendWeb.Router do
 
   pipeline :general_manager do
     plug BackendWeb.GeneralManagerPlug
-    plug CORSPlug, origin: "http://34.155.108.18:8080", headers: ["Authorization", "Content-Type", "X-Custom-Header"], methods: ["GET", "POST", "PUT", "DELETE"]
     plug :accepts, ["json"]
     plug :fetch_session
     plug :fetch_flash
@@ -26,7 +23,6 @@ defmodule BackendWeb.Router do
 
   pipeline :manager do
     plug BackendWeb.ManagerPlug
-    plug CORSPlug, origin: "http://34.155.108.18:8080", headers: ["Authorization", "Content-Type", "X-Custom-Header"], methods: ["GET", "POST", "PUT", "DELETE"]
     plug :accepts, ["json"]
     plug :fetch_session
     plug :fetch_flash
@@ -35,7 +31,6 @@ defmodule BackendWeb.Router do
 
   pipeline :default do
     plug BackendWeb.DefaultPlug
-    plug CORSPlug, origin: "http://34.155.108.18:8080", headers: ["Authorization", "Content-Type", "X-Custom-Header"], methods: ["GET", "POST", "PUT", "DELETE"]
     plug :accepts, ["json"]
     plug :fetch_session
     plug :fetch_flash
